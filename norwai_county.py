4cc6837cd0325ee94fafee809f2f831e1b815438
IntrntSrfr <aleksander@tosbakken.com>
2022-11-03 19:14:01 +0100

    added dataset, changed model

--- norwai_county.py
+++ norwai_county.py
@@ -18,58 +18,94 @@ class NSVDModel(nn.Module):
     self.conv1 = ConvModule(3, 64)
     self.conv2 = ConvModule(64, 128)
     self.conv3 = ConvModule(128, 256)
-    self.l1 = nn.Linear(256*16*16, 1024)
-    self.l2 = nn.Linear(1024, 11)
+    self.conv4 = ConvModule(256, 256)
+    self.l1 = nn.Linear(256*8*8, 512)
+    self.l2 = nn.Linear(512, 11)
     
   def forward(self, x):
     x = self.pool(self.conv1(x)) # 128 -> 64
     x = self.pool(self.conv2(x)) # 64 -> 32
     x = self.pool(self.conv3(x)) # 32 -> 16
-    x = x.reshape(-1, 256*16*16)
+    x = self.pool(self.conv4(x)) # 16 -> 8
+    x = x.reshape(-1, 256*8*8)
     x = torch.relu(self.l1(x))
     x = self.l2(x)
-    return torch.log_softmax(x, dim=1)
+    return x
+    #return torch.log_softmax(x, dim=1)
 
 if __name__ == '__main__':
-  from nsvd import NSVD_COUNTY
-  from tqdm import tqdm
+  from torch.utils.data import DataLoader
+  from torchvision import transforms, datasets, models
+  from nsvd import NSVD2
+  from tqdm import tqdm, trange
 
   device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
   print("using device:", device)
 
-  train = NSVD_COUNTY()
-  train_inputs = train.data.to(device)
-  train_targets = train.targets.to(device)
+  tf = transforms.Compose([
+    transforms.Resize((128,128)),
+    transforms.RandomHorizontalFlip(),
+    transforms.ToTensor(),
+    transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
+  ])
 
-  # normalize
-  mean = train_inputs.mean()
-  std = train_inputs.std()
-  train_inputs = (train_inputs - mean) / std
-
-  # create batches - ideally this should be a dataloader
-  batch_size = 36
-  train_input_batches = torch.split(train_inputs, batch_size)
-  train_target_batches = torch.split(train_targets, batch_size)
-
-  print("train input batch size:", train_input_batches[0].shape)
-  print("train target batch size:", train_target_batches[0].shape)
-  #print(train_targets)
+  train = NSVD2('./data', transforms=tf)
+  train_ldr = DataLoader(train, batch_size=38, shuffle=True, num_workers=4)
+  test = NSVD2('./data', train=False, transforms=tf)
+  test_ldr = DataLoader(test, batch_size=26, shuffle=True, num_workers=4)
 
   model = NSVDModel()
+  #model = models.vgg19(progress=False)
+  #model = nn.Sequential(
+  #  model, nn.Linear(1000, 11)
+  #)
   model.to(device)
 
   loss_fn = nn.CrossEntropyLoss()
   optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
+  #scheduler = torch.optim.lr_scheduler.ExponentialLR(optimizer, 0.87)
 
-  epochs = 20
+  losses = []
+  accs = []
+
+  epochs = 5
   for epoch in range(epochs):
     epoch_loss = 0
-    for batch in (pbar := tqdm(range(len(train_input_batches)), ascii=True)):
-      y = model(train_input_batches[batch])
-      loss = loss_fn(y, train_target_batches[batch])
+    pbar = trange(len(train_ldr) + len(test_ldr), ascii=True)
+    for batch_idx, (batch, labels) in enumerate(train_ldr):
+      batch, labels = batch.cuda(), labels.cuda()
+      y = model(batch)
+      loss = loss_fn(y, labels)
       loss.backward()
       optimizer.step()
       optimizer.zero_grad()
-      epoch_loss += loss
-      pbar.set_description("epoch: {}; loss: {:.5f}".format(epoch, epoch_loss/(batch+1)))
+      epoch_loss += (loss/len(train_ldr)).item()
+
+      pbar.set_description("epoch: {:2d}; loss: {:.5f}".format(epoch, epoch_loss))
+      pbar.update()
+    losses.append(epoch_loss)
+    #scheduler.step()
+
+    with torch.no_grad():
+      epoch_acc = 0
+      for batch_idx, (batch, labels) in enumerate(test_ldr):
+        batch, labels = batch.cuda(), labels.cuda()
+        y = model(batch)
+        y = torch.argmax(torch.exp(y), dim=1)
+        epoch_acc += (1-torch.count_nonzero(y-labels).item() / len(batch))/len(test_ldr)
+        pbar.set_description("epoch: {:2d}; loss: {:.5f}; acc: {:.5f}".format(epoch, epoch_loss, epoch_acc))
+        pbar.update()
+    accs.append(epoch_acc)
+    pbar.close()
   torch.save(model, './data/model_classification')
+
+  import matplotlib.pyplot as plt  
+  fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(15,5))
+  fig.set_facecolor('white')
+  ax1.set_yscale('log')
+  ax1.plot(range(len(losses)), losses, label='loss')
+  ax1.legend()
+  ax2.plot(range(len(accs)), accs, label='accuracy')
+  ax2.legend()
+  plt.show()
+
--- nsvd.py
+++ nsvd.py
@@ -16,12 +16,12 @@ class NSVD(Dataset):
     d = self.df.iloc[[idx]]
     image = Image.open(os.path.join('./data/images', d['filename'].item())).resize((128, 128))
     return np.asarray(image).transpose(2, 0, 1)
-
+''' 
 if __name__ == '__main__':
   train = NSVD()
   print(train.targets.shape)
   print(train.data.shape)
-
+ '''
 #With county
 class NSVD_COUNTY(Dataset):
   def __init__(self):
@@ -37,5 +37,33 @@ class NSVD_COUNTY(Dataset):
 
 if __name__ == '__main__':
   train = NSVD_COUNTY()
-  print(train.targets.shape)
   print(train.data.shape)
+  print(train.data.mean(dim=[0, 2, 3]))
+  #print(train.targets)
+  #print(train.data.shape)
+
+import pathlib
+
+class NSVD2(Dataset):
+  def __init__(self, root, transforms=None, train=True) -> None:
+    super(NSVD2, self).__init__()
+    self.path = pathlib.Path(root) / 'NSVD' / ('train' if train else 'test')
+    self.files = list(self.path.glob('*/*.jpg'))
+    self.classes = [d.name for d in self.path.iterdir() if d.is_dir()]
+    self.idx_to_class = {i:v for i, v in enumerate(self.classes)}
+    self.class_to_idx = {v:i for i, v in enumerate(self.classes)}
+
+    self.transforms = transforms
+
+  def __len__(self):
+    return len(self.files)
+
+  def __getitem__(self, idx):
+    f = self.files[idx]
+    label = self.class_to_idx[f.parent.name]
+    img = Image.open(f)
+
+    if self.transforms:
+      img = self.transforms(img)
+    return img, label
+  
