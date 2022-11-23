5fbd530734e3e77f1d642d271070f0eafabd7303
IntrntSrfr <aleksander@tosbakken.com>
2022-11-23 16:05:47 +0100

    several things

--- norwai_county.py
+++ norwai_county.py
@@ -1,5 +1,6 @@
 import torch
 from torch import nn 
+from torchvision import models
 
 class ConvModule(nn.Module):
   def __init__(self, in_features, out_features) -> None:
@@ -12,112 +13,83 @@ class ConvModule(nn.Module):
     return torch.relu(x)
 
 class NSVDModel(nn.Module):
-  def __init__(self) -> None:
+  def __init__(self, num_classes) -> None:
     super(NSVDModel, self).__init__()
-    
+    self.num_classes = num_classes
+
     self.features = nn.Sequential(
       ConvModule(3, 64),
+      nn.MaxPool2d(2), # 224 -> 112
       ConvModule(64, 128),
-      nn.MaxPool2d(2), # 512 -> 256
+      nn.MaxPool2d(2), # 112 -> 56
       ConvModule(128, 256),
-      ConvModule(256, 512),
-      nn.MaxPool2d(2), # 256 -> 128
-      ConvModule(512, 512),
-      ConvModule(512, 512),
-      nn.MaxPool2d(2), # 128 -> 64
-      ConvModule(512, 512),
-      ConvModule(512, 512),
-      nn.MaxPool2d(2), # 64 -> 32
-      ConvModule(512, 512),
-      ConvModule(512, 512),
-      nn.MaxPool2d(2), # 32 -> 16
-      ConvModule(512, 512),
-      ConvModule(512, 512),
-      nn.MaxPool2d(2), # 16 -> 8
+      nn.MaxPool2d(2), # 56 -> 28
+      ConvModule(256, 256),
+      nn.MaxPool2d(2), # 28 -> 14
+      ConvModule(256, 256),
+      nn.MaxPool2d(2), # 14 -> 7
     )
     
     self.classifier = nn.Sequential(
-      nn.Linear(512*8*8, 1024),
+      nn.Linear(256*7*7, 512),
+      nn.BatchNorm1d(512),
       nn.ReLU(True),
-      nn.Linear(1024, 1024),
+      nn.Dropout(),
+      nn.Linear(512, 512),
+      nn.BatchNorm1d(512),
       nn.ReLU(True),
-      nn.Linear(1024, 10),
+      nn.Dropout(),
+      nn.Linear(512, self.num_classes),
     )
-    
+  
   def forward(self, x: torch.Tensor):
     x = self.features(x)
     x = torch.flatten(x, 1)
     x = self.classifier(x)
     return x
 
+  def __len__(self):
+    return sum(p.numel() for p in self.parameters())
+
+
 if __name__ == '__main__':
-  from torch.utils.data import DataLoader
-  from torchvision import transforms, models
-  from nsvd import NSVD4
-  from tqdm import tqdm
-  import pandas as pd
+  model = NSVDModel(10)
+  print("params:", len(model))
 
-  device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-  print("using device:", device)
 
-  tf = transforms.Compose([
-    transforms.Resize((512, 512)),
-    transforms.RandomHorizontalFlip(),
-    transforms.ToTensor(),
-    transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
-  ])
+def set_requires_grad(model: nn.Module, feature_extract: bool):
+  if feature_extract:
+    for param in model.parameters():
+      param.requires_grad = False
 
-  train = NSVD4('./data', True, 'county', False, transforms=tf)
-  train_ldr = DataLoader(train, batch_size=32, shuffle=True, num_workers=4)
-  test = NSVD4('./data', False, 'county', False, transforms=tf)
-  test_ldr = DataLoader(test, batch_size=32, shuffle=True, num_workers=4)
+def get_model(architecture: str, num_classes: int, feature_extract: bool, use_pretrained=True) -> nn.Module | None:
+  model = None
+  
+  if architecture == "NSVD":
+    return NSVDModel(num_classes)
 
-  print("training data: {} images, {} batches".format(len(train), len(train_ldr)))
-  print("testing data: {} images, {} batches".format(len(test), len(test_ldr)))
+  elif architecture == "ResNet50":
+    weights = models.ResNet50_Weights.DEFAULT
+    model = models.resnet50(progress=False, weights=weights if use_pretrained else None)
+    set_requires_grad(model, feature_extract)
+    model.fc = nn.Linear(model.fc.in_features, num_classes)
 
-  #model = NSVDModel()
-  model = models.resnet152(progress=False, weights=models.ResNet152_Weights.IMAGENET1K_V1)
-  model.fc = nn.Linear(512, 10)
-  #for i, param in enumerate(model.features.parameters()):
-  #  param.requires_grad = False
-  #model.classifier[6] = nn.Linear(4096, 10)
-  model.to(device)
-  model_name = "resnet152_512_10"
+  elif architecture == "ResNet152":
+    weights = models.ResNet152_Weights.DEFAULT
+    model = models.resnet152(progress=False, weights=weights if use_pretrained else None)
+    set_requires_grad(model, feature_extract)
+    model.fc = nn.Linear(model.fc.in_features, num_classes)
 
-  loss_fn = nn.CrossEntropyLoss()
-  optimizer = torch.optim.Adam(model.parameters(), lr=0.00001, weight_decay=1e-4)
-  scheduler = torch.optim.lr_scheduler.ExponentialLR(optimizer, 0.95)
+  elif architecture == "vgg19":
+    weights = models.VGG19_BN_Weights.DEFAULT
+    model = models.vgg19_bn(progress=False, weights=weights if use_pretrained else None)
+    set_requires_grad(model, feature_extract)
+    model.classifier[6] = nn.Linear(4096, num_classes)
 
-  losses = []
-  accs = []
+  elif architecture == "EfficientNet":
+    weights = models.EfficientNet_V2_S_Weights.DEFAULT
+    model = models.efficientnet_v2_s(progress=False, weights=weights if use_pretrained else None)
+    set_requires_grad(model, feature_extract)
+    model.classifier[1] = nn.Linear(model.classifier[1].in_features, num_classes)
 
-  epochs = 10
-  for epoch in range(epochs):
-    print("epoch: {}; lr: {}".format(epoch, scheduler.get_last_lr()[0]))
-    epoch_loss = 0
-    for batch_idx, (batch, labels) in (pbar := tqdm(enumerate(train_ldr), total=len(train_ldr))):
-      batch, labels = batch.to(device), labels.to(device)
-      y = model(batch)
-      loss = loss_fn(y, labels)
-      loss.backward()
-      optimizer.step()
-      optimizer.zero_grad()
-      epoch_loss += loss.item() / len(train_ldr)
-
-      pbar.set_description("    loss: {:.5f}".format(epoch_loss))
-    losses.append(epoch_loss)
-    scheduler.step()
-
-    with torch.no_grad():
-      epoch_acc = 0
-      for batch_idx, (batch, labels) in (pbar := tqdm(enumerate(test_ldr), total=len(test_ldr))):
-        batch, labels = batch.to(device), labels.to(device)
-        y = model(batch)
-        y = torch.argmax(torch.exp(y), dim=1)
-        epoch_acc += (1 - torch.count_nonzero(y - labels).item() / len(batch)) / len(test_ldr)
-        pbar.set_description("    acc: {:.5f}".format(epoch_acc))
-    accs.append(epoch_acc)
-
-  torch.save(model, './data/trained_models/county/{}'.format(model_name))
-  df = pd.DataFrame({'accuracy': accs, 'losses': losses})
-  df.to_csv('./data/trained_models/county/metrics/{}.csv'.format(model_name), index=False)
+  return model
--- nsvd.py
+++ nsvd.py
@@ -1,108 +1,25 @@
-import torch
 from torch.utils.data import Dataset
 from PIL import Image
 import pandas as pd
-import numpy as np
 import pathlib
-import os
 import matplotlib.pyplot as plt
 
+IDX2COUNTY = {
+  0: 'agder',
+  1: 'troms og finnmark',
+  2: 'møre og romsdal',
+  3: 'vestfold og telemark',
+  4: 'trøndelag',
+  5: 'rogaland',
+  6: 'innlandet',
+  7: 'viken',
+  8: 'nordland',
+  9: 'vestland',
+}
 
 class NSVD(Dataset):
-  def __init__(self):
-    self.df = pd.read_csv('./data/locations.csv').iloc[:3600]
-    self.targets = torch.tensor(self.df[['lat', 'lng']].to_numpy()).float()
-    self.data = torch.tensor(np.array([self[idx] for idx in range(len(self.df))])).float()#torch.tensor([self[idx] for idx in range(5)])
-  def __len__(self):
-    return len(self.df)
-  def __getitem__(self, idx):
-    d = self.df.iloc[[idx]]
-    image = Image.open(os.path.join('./data/images', d['filename'].item())).resize((128, 128))
-    return np.asarray(image).transpose(2, 0, 1)
-
-class NSVD_COUNTY(Dataset):
-  def __init__(self):
-    self.df = pd.read_csv('./data/locations_county.csv').iloc[:3600]
-    lst = os.listdir("./data/images")
-    self.df = self.df[self.df['filename'].str.contains('|'.join(lst)) == True]
-    self.targets = torch.tensor(self.df["fylkesnummer"].to_numpy()).long()
-    self.data = torch.tensor(np.array([self[idx] for idx in range(len(self.df))])).float()#torch.tensor([self[idx] for idx in range(5)])
-  def __len__(self):
-    return len(self.df)
-  def __getitem__(self, idx):
-    d = self.df.iloc[[idx]]
-    image = Image.open(os.path.join('./data/images', d['filename'].item())).resize((256, 256))
-    return np.asarray(image).transpose(2, 0, 1)
-
-
-class NSVD2(Dataset):
-  def __init__(self, root, transforms=None, train=True) -> None:
-    super(NSVD2, self).__init__()
-    self.path = pathlib.Path(root) / 'NSVD' / ('train' if train else 'test')
-    self.files = list(self.path.glob('*/*.jpg'))
-    self.classes = [d.name for d in self.path.iterdir() if d.is_dir()]
-    self.idx_to_class = {i:v for i, v in enumerate(self.classes)}
-    self.class_to_idx = {v:i for i, v in enumerate(self.classes)}
-
-    self.transforms = transforms
-
-  def __len__(self):
-    return len(self.files)
-
-  def __getitem__(self, idx):
-    f = self.files[idx]
-    label = self.class_to_idx[f.parent.name]
-    img = Image.open(f)
-
-    if self.transforms:
-      img = self.transforms(img)
-    return img, label
-
-class NSVD3(Dataset):
-  def __init__(self, root, transforms=None, train=True) -> None:
-    super(NSVD3, self).__init__()
-    self.path = pathlib.Path(root) / 'NSVD' 
-    self.df = pd.read_csv(self.path / 'data.csv')
-    self.files = list((self.path / ('train' if train else 'test')).glob('*.jpg'))
-    self.transforms = transforms
-  
-  def __len__(self):
-    return len(self.files)
-  
-  def __getitem__(self, index):
-    f = self.files[index]
-    label = self.df.loc[self.df['filename'] == f.name].to_dict(orient='records')[0]["county"]
-    img = Image.open(f)
-
-    if self.transforms:
-      img = self.transforms(img)
-    return img, label
-
-class NSVD_Boxes(Dataset):
-  def __init__(self, root, transforms=None, train=True) -> None:
-    super(NSVD_Boxes, self).__init__()
-    self.path = pathlib.Path(root) / 'NSVD' 
-    self.df = pd.read_csv(self.path / 'data_boxes.csv')
-    self.files = list((self.path / ('train' if train else 'test')).glob('*.jpg'))
-    #Remove all files that are not in df
-    self.files = [f for f in self.files if f.name in self.df['filename'].values]
-    self.transforms = transforms
-  
-  def __len__(self):
-    return len(self.files)
-  
-  def __getitem__(self, index):
-    f = self.files[index]
-    label = self.df.loc[self.df['filename'] == f.name].to_dict(orient='records')[0]
-    img = Image.open(f)
-
-    if self.transforms:
-      img = self.transforms(img)
-    return img, label
-
-class NSVD4(Dataset):
   def __init__(self, root, train, label, return_coords, transforms=None):
-    super(NSVD4, self).__init__()
+    super(NSVD, self).__init__()
 
     if label not in ['county', 'coords']: # add this later ", 'zone']:"
       raise ValueError('label must be one in ["county", "coords", "zone"]')
@@ -133,6 +50,63 @@ class NSVD4(Dataset):
       return img, label, row[['lat', 'lng']].to_numpy()[0]
     return img, label, -1
 
+class NSVD_B(Dataset):
+  def __init__(self, root, train, label, return_coords, transforms=None):
+    super(NSVD_B, self).__init__()
+
+    if label not in ['county', 'coords']: # add this later ", 'zone']:"
+      raise ValueError('label must be one in ["county", "coords", "zone"]')
+    self.label = label
+    
+    self.path = pathlib.Path(root) / 'NSVD_B' 
+    self.df = pd.read_csv(self.path / 'data.csv')
+    self.files = list((self.path / ('train' if train else 'test')).glob('*.jpg'))
+
+    self.transforms = transforms
+    self.return_coords = return_coords
+  
+  def __len__(self):
+    return len(self.files)
+
+  def __getitem__(self, index):
+    f = self.files[index]
+    row = self.df.loc[self.df['filename'] == f.name]
+    if self.label == 'coords':
+      label = self.df.loc[self.df['filename'] == f.name][['lat', 'lng']].to_numpy()[0]
+    else:
+      label = self.df.loc[self.df['filename'] == f.name][[self.label]].to_numpy()[0][0]
+    img = Image.open(f)
+    if self.transforms:
+      img = self.transforms(img)
+    
+    if self.return_coords:
+      return img, label, row[['lat', 'lng']].to_numpy()[0]
+    return img, label, -1
+
+
+class NSVD_Boxes(Dataset):
+  def __init__(self, root, transforms=None, train=True) -> None:
+    super(NSVD_Boxes, self).__init__()
+    self.path = pathlib.Path(root) / 'NSVD' 
+    self.df = pd.read_csv(self.path / 'data_boxes.csv')
+    self.files = list((self.path / ('train' if train else 'test')).glob('*.jpg'))
+    #Remove all files that are not in df
+    self.files = [f for f in self.files if f.name in self.df['filename'].values]
+    self.transforms = transforms
+  
+  def __len__(self):
+    return len(self.files)
+  
+  def __getitem__(self, index):
+    f = self.files[index]
+    label = self.df.loc[self.df['filename'] == f.name].to_dict(orient='records')[0]
+    img = Image.open(f)
+
+    if self.transforms:
+      img = self.transforms(img)
+    return img, label
+
+
 
 def plot_img(t):
   t = t.numpy().transpose((1,2,0))
@@ -149,7 +123,7 @@ if __name__ == '__main__':
     transforms.ToTensor(),
   ])
 
-  d = NSVD4('data', True, "county", True, transforms=tf)
+  d = NSVD('data', True, "county", True, transforms=tf)
 
   img, label, coords = d[random.randint(0, len(d)-1)]
   print(label)
