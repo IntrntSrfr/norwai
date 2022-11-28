0523a4de4021f63fb94b37acb562239d2e5a9f6a
XkijuX <sanderhegland@gmail.com>
2022-11-28 08:46:32 +0100

    Added norwai regression single training file

--- norwai_regression.py
+++ norwai_regression.py
@@ -1,58 +1,85 @@
 import torch
 from torch import nn
+from torch.utils.data import DataLoader
+from torchvision import transforms
+from tqdm import tqdm
 
-class ConvModule(nn.Module):
-  def __init__(self, in_features, out_features) -> None:
-    super(ConvModule, self).__init__()
-    self.conv = nn.Conv2d(in_features, out_features, 5, padding=2)
-    self.norm = nn.BatchNorm2d(out_features)
+from util import make_data_folders
+from norwai_models import get_model, Haversine
+from nsvd import NSVD, NSVD_B
 
-  def forward(self, x) -> torch.Tensor:
-    x = self.norm(self.conv(x))
-    return torch.relu(x)
+make_data_folders()
 
-class NSVDModel(nn.Module):
-  def __init__(self) -> None:
-    super(NSVDModel, self).__init__()
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-    self.features = nn.Sequential(
-      ConvModule(3, 64),
-      nn.MaxPool2d(2), # 224 -> 112
-      ConvModule(64, 128),
-      nn.MaxPool2d(2), # 112 -> 56
-      ConvModule(128, 256),
-      nn.MaxPool2d(2), # 56 -> 28
-      ConvModule(256, 256),
-      nn.MaxPool2d(2), # 28 -> 14
-      ConvModule(256, 256),
-      nn.MaxPool2d(2), # 14 -> 7
-    )
-    
-    self.classifier = nn.Sequential(
-      nn.Linear(256*7*7, 512),
-      nn.BatchNorm1d(512),
-      nn.ReLU(True),
-      nn.Linear(512, 512),
-      nn.BatchNorm1d(512),
-      nn.ReLU(True),
-      nn.Linear(512, 2),
-    )
-  
-  def forward(self, x: torch.Tensor):
-    x = self.features(x)
-    x = torch.flatten(x, 1)
-    x = self.classifier(x)
-    return x
+#Variables
+architecture = "NSVD" 
+pretrained = True
+epochs = 10
+lr = 0.001
+lr_decay = 0.95
+batch_size = 32
+dataset = 'full' # 'full' or 'balanced'
 
-class Haversine(nn.Module):
-  def __init__(self) -> None:
-    super(Haversine, self).__init__()
-  
-  def forward(self, lat1: torch.Tensor, lng1: torch.Tensor, lat2: torch.Tensor, lng2: torch.Tensor) -> torch.Tensor:
-    lng1, lat1, lng2, lat2 = map(torch.deg2rad, [lng1, lat1, lng2, lat2])
-    dlng = lng2 - lng1
-    dlat = lat2 - lat1
-    a = torch.sin(dlat/2.0)**2 + torch.cos(lat1) * torch.cos(lat2) * torch.sin(dlng/2.0)**2
-    c = 2 * torch.arcsin(torch.sqrt(a))
-    km = 6371 * c
-    return km.mean()
+tf_train = transforms.Compose([
+  transforms.RandomResizedCrop((224, 224), (0.7, 1)),
+  transforms.RandomHorizontalFlip(),
+  transforms.ToTensor(),
+  transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
+])
+
+tf_test = transforms.Compose([
+  transforms.Resize((224, 224)),
+  transforms.ToTensor(),
+  transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
+])
+
+def get_dataset(full):
+  if full:
+    return NSVD('./data', True,  "coords", False, transforms=tf_train), NSVD('./data', False,  "coords", False, transforms=tf_train)
+  else:
+    return NSVD_B('./data', True,  "coords", False, transforms=tf_train), NSVD_B('./data', False,  "coords", False, transforms=tf_train)
+
+train_data, test_data = get_dataset(dataset == 'full')
+
+model = get_model(architecture, 2, False, pretrained)
+assert model is not None, "Input architecture is invalid"
+model = nn.DataParallel(model)
+model.to(device)
+
+train_ldr = DataLoader(train_data, batch_size=batch_size, shuffle=True, num_workers=4)
+test_ldr = DataLoader(test_data, batch_size=batch_size, shuffle=True, num_workers=4)
+
+loss_fn = Haversine()
+optimizer = torch.optim.Adam(model.parameters(), lr=lr)
+scheduler = torch.optim.lr_scheduler.StepLR(optimizer, 1, 1)
+if lr_decay:
+  scheduler = torch.optim.lr_scheduler.ExponentialLR(optimizer, lr_decay)
+
+for epoch in range(epochs):
+  print("epoch: {}; lr: {}".format(epoch, scheduler.get_last_lr()[0]))
+  model.train()
+  train_loss = 0
+  for batch_idx, (batch, labels, _) in (pbar := tqdm(enumerate(train_ldr), total=len(train_ldr))):
+    batch, labels = batch.to(device), labels.to(device).float()
+    y = model(batch)
+    loss = loss_fn(y[:, 0], y[:, 1], labels[:, 0], labels[:, 1])
+    loss.backward()
+    optimizer.step()
+    optimizer.zero_grad()
+    train_loss += loss.item()
+    pbar.set_description("    loss: {:.2f}".format(train_loss / (batch_idx + 1)))
+
+  with torch.no_grad():
+    model.eval()
+    test_loss = 0
+    for batch_idx, (batch, labels, _) in (pbar := tqdm(enumerate(test_ldr), total=len(test_ldr))):
+      batch, labels = batch.to(device), labels.to(device)
+      y = model(batch)
+      loss = loss_fn(y[:, 0], y[:, 1], labels[:, 0], labels[:, 1])
+      test_loss += loss.item()
+      pbar.set_description("    acc: {:.2f}".format(test_loss / (batch_idx + 1)))
+  scheduler.step()
+
+#Save the model
+torch.save(model, './data/norwai_regression.model')
